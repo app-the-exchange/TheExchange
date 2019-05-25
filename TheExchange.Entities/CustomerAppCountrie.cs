212507c7f809d149e936e6d2cfa97c5c581dbc38
Jonatas Dart <jonatas.aguiar@dartdigital.com.br>
2019-05-25 12:25:47 -0300

    Add JsonIgnore

--- TheExchange.Entities/CustomerAppCountrie.cs
+++ TheExchange.Entities/CustomerAppCountrie.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace TheExchange.Entities
 {
     public class CustomerAppCountrie
     {
+        public int idcustomer_app_countries { get; set; }
+        public int idcustomer_app { get; set; }
+        public int idcountry { get; set; }
+
+        [JsonIgnore]
+        public virtual Country country { get; set; }
+
+        [JsonIgnore]
+        public virtual CustomerApp customer_app { get; set; }
     }
 }
